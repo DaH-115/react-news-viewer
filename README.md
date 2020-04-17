0f843aa544ef93dfd6d59013e307c52e27093b42
da-yooom <kdh941110@gmail.com>
2020-04-17 20:21:14 +0900

    API를 호출하여 데이터를 볼러 옴

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ## 전 세계의 뉴스를 찾아볼 수 있는 News API
 
 https://newsapi.org/s/south-korea-news-api
+
+<br>
+
+### API를 연동할 때 주의할 점
+
+- 컴포넌트가 화면에 보이는 시점에 API를 요청할 때 useEffect를 사용하여 컴포넌트가 처음 렌더링되는 시점에 API를 요청하면 된다.<br>
+  여기서 주의해야 할 점은 useEffect에 등록하는 함수에 async를 붙이면 안 된다. useEffect에서 반환해야 하는 값은<br>
+  뒷정리 함수이기 때문이다. 따라서 useEffect 내부에서 async/await를 사용하고 싶다면, 함수 내부에 async 키워드가 붙은<br>
+  또 다른 함수를 만들어서 사용해야 한다.
+
+- 데이터를 불러와서 뉴스 데이터 배열을 map 함수를 사용하여 컴포넌트 배열로 변환할 때 반드시 !aricles를 조회하여<br>
+  해당 값이 현재 null 이 아닌지 검사해야 한다. 데이터가 유효하지 않다면, null 에는 map 함수가 없기 때문에 렌더링 과정에서<br>
+  오류가 발생하여 빈 화면만 보이게 된다
+
+```jsx
+// 아직 articles 값이 설정되지 않았을 때 (map 함수 사용하기 전 검사)
+if (!articles) {
+  return null;
+}
+```
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import axios from "axios";
 
+// STYLE
 const newsListBlock = styled.div`
   box-sizing: border-box;
   padding-bottom: 3rem;
@@ -15,22 +17,41 @@ const newsListBlock = styled.div`
   }
 `;
 
-const sampleArticle = {
-  title: "제목",
-  description: "내용",
-  url: "https://google.com",
-  urlToImage: "https://via.placeholder.com/160",
-};
-
 function NewsList() {
+  const [articles, setArticles] = useState(null); // API 로 받아오는 값
+  const [loading, setLoading] = useState(false); // Loading 상태 관리
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://newsapi.org/v2/top-headlines?country=kr&apiKey=fa70351bae8948279923fab34d45d5aa"
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  // 대기(로딩) 중일 때
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  // 아직 articles 값이 설정되지 않았을 때 (map 함수 사용하기 전 검사)
+  if (!articles) {
+    return null;
+  }
+
+  // articles 값이 유효할 때
   return (
     <newsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </newsListBlock>
   );
 }
